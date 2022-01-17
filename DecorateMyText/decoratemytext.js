6f615ef70f10d5d901a288e7e90fb8c5294e2e66
psina12 <sinapandey4@gmail.com>
2022-01-16 23:28:30 -0600

    DecorateMyText/

---
+++ DecorateMyText/decoratemytext.js
@@ -0,0 +1,38 @@
+
+
+
+function biggerDecorations() {
+    
+    var text = document.getElementById("inputTextArea");
+    var size = window.getComputedStyle(text).fontSize;
+    size = parseInt(size);
+    size = size + 2;
+    text.style.fontSize = size + "pt";
+
+}
+
+function setTimer() {
+    setInterval(biggerDecorations, 3000);
+
+}
+
+function blingCheckbox(checkbox) {
+    var text = document.getElementById("inputTextArea").style;
+    if (checkbox.checked) {
+        text.fontWeight = "bold";
+        text.color = "green";
+        text.textDecoration = "underline";
+        document.body.style.background = "#f3f3f3 url('https://courses.cs.washington.edu/courses/cse190m/12su/labs/6/hundred-dollar-bill.jpg')  no-repeat right top";
+
+
+    } else {
+        text.fontWeight = "normal";
+        text.color = "black";
+        text.textDecoration = "none";
+        document.body.style.background = "none";
+    }
+
+}
+
+
+
